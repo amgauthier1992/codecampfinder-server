8180aabe42b2698df811b1487efb0257d37a48ab
Andy Gauthier <amgauthier1992@gmail.com>
2020-12-16 11:28:57 -0500

    updated for TC

--- migrations/010.do.create.course_frameworks.sql
+++ migrations/010.do.create.course_frameworks.sql
@@ -1,5 +1,5 @@
 CREATE TABLE course_frameworks (
-  course_id INTEGER INTEGER REFERENCES bootcamp_courses(id),
-  framework_id INTEGER INTEGER REFERENCES frameworks(id),
+  course_id INTEGER REFERENCES bootcamp_courses(id),
+  framework_id INTEGER REFERENCES frameworks(id),
   PRIMARY KEY (course_id, framework_id)
 );
