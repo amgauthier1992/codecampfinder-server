073508091592f73fdec5cb02545807221df4ced4
Andy Gauthier <amgauthier1992@gmail.com>
2020-12-16 11:24:21 -0500

    updated for TC

--- migrations/011.do.create.user_courses.sql
+++ migrations/011.do.create.user_courses.sql
@@ -1,5 +1,5 @@
 CREATE TABLE user_courses (
-  course_id INTEGER REFERENCES courses(id),
+  course_id INTEGER REFERENCES bootcamp_courses(id),
   user_name TEXT NOT NULL REFERENCES users(user_name),
   PRIMARY KEY (course_id, user_name)
 );
